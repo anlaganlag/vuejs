084c6ac023e60078a9be45c3d16117d8f49fbb7c
anlaganlag <tanjhu@163.com>
2021-02-28 22:54:22 +0800

    计算器vue项目

---
+++ cal/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <Xi />
+  </div>
+</template>
+
+<script>
+import Xi from './components/Cal.vue'
+
+export default {
+  name: "App",
+  components: {
+    Xi,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
