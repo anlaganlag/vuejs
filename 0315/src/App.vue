0196b477d44ae472273451cfa3acc4f0eb4c039a
anlaganlag <tanjhu@163.com>
2021-03-15 12:34:36 +0800

    vuex的store最简单的用法

---
+++ 0315/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <h2>{{ $store.state.count }}</h2>
+    <button @click="increment">+</button>
+    <button @click="decrement">-</button>
+    <hello-world />
+    <!-- {{ store.state.count }} -->
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld";
+export default {
+  name: "App",
+
+  methods: {
+    increment() {
+      this.$store.commit("increment");
+      console.log(this.$store.state.count);
+    },
+    decrement() {
+      this.$store.commit("decrement");
+      console.log(this.$store.state.count);
+    },
+  },
+  components: { HelloWorld },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
