66fa30e1e3ee71e74175fec3cbeb23292fb6a5f4
anlaganlag <tanjhu@163.com>
2021-03-16 19:05:51 +0800

    优化crud项目

--- crud0316/src/App.vue
+++ crud0316/src/App.vue
@@ -1,41 +1,27 @@
 <template>
   <div id="app">
-    <HelloWorld
+    <CRUD
       :items="items"
-      :create="create"
       :msg="msg"
-      :update="update"
-      :del="del"
     />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import CRUD from "./components/CRUD.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    CRUD,
   },
   data() {
     return {
-      items: ["alex", "AAA", "bbb", "ccc"],
+      items: ["python", "javascrip", "Backend", "Frontend"],
       msg: "",
     };
   },
-  methods: {
-    create(text) {
-      this.items.push(text);
-    },
-    update(idx, msg) {
-      this.items.splice(idx, 1, msg);
-    },
-    del(idx) {
-      console.log(idx);
-      this.items.splice(idx, 1);
-    },
-  },
+
 };
 </script>
 
---
+++ crud0316/src/components/CRUD.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="crud">
+    <input v-model="msg" />
+    <button @click="create(msg)">Create</button>
+    <p v-for="(i, idx) in items" :key="i">
+      {{ i }}
+      <button @click="update(idx, msg)">Update</button>
+      <button @click="del(idx)">Delete</button>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CRUD",
+  props: {
+    items: Array,
+    msg: String,
+  },
+  methods: {
+    create(text) {
+      this.items.push(text);
+    },
+    update(idx, msg) {
+      this.items.splice(idx, 1, msg);
+    },
+    del(idx) {
+      this.items.splice(idx, 1);
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
