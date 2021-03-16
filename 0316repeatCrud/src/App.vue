8dc516fb4ebb498a888f01b5ee5c8f5016207adb
anlaganlag <tanjhu@163.com>
2021-03-16 20:32:38 +0800

    使用v-for在组件里实现

---
+++ 0316repeatCrud/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <input v-model="msg" />
+    <button @click="create(msg)">Create</button>
+
+    <CRUD v-for="(item,idx) in items" :msg="msg" :key="item.id" :item="item" :idx="idx" @del="del"/>
+  </div>
+</template>
+
+<script>
+import CRUD from "./components/CRUD.vue";
+
+export default {
+  name: "App",
+  components: {
+    CRUD,
+  },
+  data() {
+    return {
+      msg: "",
+      items: ["r","g","b"],
+      postFontSize: 1,
+    };
+  },
+  methods: {
+    create(text) {
+      this.items.push(text);
+    },
+    del(idx){
+      this.items.splice(idx,1)
+    }
+  },
+};
+</script>
